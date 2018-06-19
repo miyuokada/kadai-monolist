a9dbb310e93f370fe689b5db7961815ba2543fc0
EC2 Default User <ec2-user@ip-172-31-5-29.us-east-2.compute.internal>
2018-06-19 03:14:56 +0000

    kadai

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,9 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 
 Route::group(['middleware' => ['auth']], function () {
+    
+    Route::post('have', 'ItemUserController@have')->name('item_user.have');
+    Route::delete('have', 'ItemUserController@dont_have')->name('item_user.dont_have');
     Route::resource('items', 'ItemsController', ['only' => ['create', 'show']]);
     Route::post('want', 'ItemUserController@want')->name('item_user.want');
     Route::delete('want', 'ItemUserController@dont_want')->name('item_user.dont_want');
